dc0a8fefcb29c2458277fd37cc1cddb28d6fa571
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-01 15:07:38 +0200

    added WeatherCard content

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {Card} from '../ui/Card'
 import { Text } from '../ui/Text'
-import { Thermometer } from 'lucide-react'
+
 
 export const WeatherCard = () => {
   return (
-    <Card className='bg-gradient-to-r from-sky-500 to-cyan-400'>
-            <Text variant={'h1'}>Durban</Text>
+    <Card className='bg-gradient-to-r from-sky-500 to-cyan-400 mt-5 w-150 h-100'>
+            <Text variant={'h1'} className='text-base color-red'>Durban</Text>
             <Text variant={'p'}>Saturday, 17 May</Text>
             <Text variant={'p'}>14:30</Text>
-            <Text variant={'h2'}>23 <Thermometer /></Text>
+            <Text variant={'h2'}>23 {"\u00B0"} c</Text>
         
   
         
