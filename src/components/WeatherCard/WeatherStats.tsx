946c44beda31f8de64ad8c221b57533b09b6a1c4
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-08 14:12:56 +0200

    Styled the content to display pleasantly.

--- src/components/WeatherCard/WeatherStats.tsx
+++ src/components/WeatherCard/WeatherStats.tsx
@@ -12,20 +12,20 @@ interface StatsProps{
 
 export const WeatherStats:React.FC<StatsProps> = ({weather}) => {
   return (
-    <Card className='absolute bottom-4 w-95 left-1 h-25 bg-black-400'>
+    <div className='absolute bottom-4 w-95 left-1 h-25 bg-black-400 p-4'>
      
-        <div className='flex gap-18 -mt-6'>
+        <div className='flex gap-35 -mt-6'>
              <Text variant={'span'} className='text-white'>Humidity <br />{weather.humidity}</Text>
              <Text variant={'span'} className='text-white'>Wind <br />{weather.windspeed}</Text>
              <Text variant={'span'} className='text-white'>Temp <br />{weather.temp}</Text>
         </div>
 
-           <div className='flex gap-30 mt-3'>
+           <div className='flex gap-48 mt-3'>
             <Text variant={'span'} className='text-white'><Droplets size={15}/></Text>
             <Text variant={'span'} className='flex text-white '><Wind size={15}/></Text>
             <Text variant={'span'} className='text-white'><Thermometer size={15}/></Text>
         </div>
       
-    </Card>
+    </div>
   )
 }
