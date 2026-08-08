34a8fb60f541fcc2bb713d05ae756bc87429939c
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-08 14:08:00 +0200

    Cut days to be displayed halfly, and added icons

--- src/components/WeatherCard/DailyForecast.tsx
+++ src/components/WeatherCard/DailyForecast.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text } from '../ui/Text'
 import { Card } from '../ui/Card'
 import type { WeatherData } from '../../types/Weather'
+import { WeatherIcons } from '../utils/weatherIcons'
 
 export interface DailyProps {
   weather: WeatherData
@@ -12,14 +13,14 @@ export interface DailyProps {
 
 export const DailyForecast: React.FC<DailyProps> = ({ weather }) => {
   return (
-    <Card className='h-40 p-4 w-195 mb-2 bg-[#343A46] overflow-x-auto  scrollbar-track-[#343A46] scrollbar-thumb-sky-300'>
+    <Card className='h-40 p-4 w-300 mb-2 bg-[#343A46] '>
       <div className='flex gap-10'>
-        {weather.days?.map((day) => (
-          <div key={day.datetime}>
-            <Text variant='p' className='text-white'>{new Date(day.datetime!).toLocaleDateString('en-US', { weekday: 'long' })}</Text>
-            <Text variant='p' className='text-white'>{day.icon}</Text>
-            <Text variant='p' className='text-white'>{day.tempMax}</Text>
-            <Text variant='p' className='text-white'> {day.tempMin}</Text>
+        {weather.days?.slice(0, 11).map((day) => (
+          <div key={day.datetime} className='spce-y-6 p-4'>
+            <Text variant='p' className='text-white text-lg'>{new Date(day.datetime!).toLocaleDateString('en-US', { weekday: 'short' })}</Text>
+            <Text variant={'p'} className='text-white'>{WeatherIcons(day.icon ?? 'clear-day')}</Text>
+            <Text variant='p' className='text-white'>{day.tempmax}{"\u00B0"}</Text>
+            <Text variant='p' className='text-white'> {day.tempmin}{"\u00B0"}</Text>
           </div>
         ))}
       </div>
