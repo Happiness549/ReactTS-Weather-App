b246b48958fba293166b012e99d70ebb32d00c2f
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-08-08 14:11:41 +0200

    Changed hard coded icon to be that of an api and styled the content to display nicely on the card

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -3,6 +3,7 @@ import {Card} from '../ui/Card'
 import { Text } from '../ui/Text'
 import { Sun } from 'lucide-react'
 import { Button } from '../ui/Button'
+import { WeatherIcons } from '../utils/weatherIcons'
 import type { WeatherData } from '../../types/Weather'
 
  type WeatherProps ={
@@ -14,17 +15,21 @@ export const WeatherCard:React.FC<WeatherProps> = ({weather}) => {
   if (!weather) return null
 
   return (
-    <Card className='bg-[#343A46] to-cyan-400 w-100 h-100 -mt-4 mb-3'>
+    <Card className='bg-[#343A46] to-cyan-400 w-150 h-100 -mt-4 mb-3'>
             <Text variant={'h1'} className='text-6xl text-white'>{weather.address}</Text> 
             <div className='flex gap-40 mt-2'>
               
-              <Text variant={'p'} className='text-white'>{weather.datetime}</Text>
-              <Sun className="text-yellow-500 fill-yellow-200" size={62} 
-      />
+              <Text variant={'p'} className='text-white mt-10'>{weather.datetime}</Text>
+              <Text variant={'p'} className='!text-7xl ml-30'>{WeatherIcons(weather.icon)}</Text> 
+      
             </div>
             
             <Text variant={'p'} className='text-white'>{weather.time}</Text>
-             <Text variant={'h2'} className='text-5xl text-white'>{weather.temp} {"\u00B0"}c</Text> 
+             <Text variant={'h2'} className='text-5xl text-white mt-7'>{weather.temp} {"\u00B0"}c</Text>
+             
+             
+             
+
     </Card>
   )
 }
